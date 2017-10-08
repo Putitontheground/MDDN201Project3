70065766351493dacbab772d039aa6e6baa1768e
Putitontheground <ejsmith9725@gmail.com>
2017-10-09 12:51:19 +1300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,8 +1,31 @@
 
-var No = 100, tr = 8;
 var roun, r, t;
-var speed = 1/10000;
 var counter = 0;
+
+
+//dat.GUI controls
+
+var gui = new dat.GUI();
+
+var obj = {
+  size: 150,
+  amount: 200,
+  speed: 10,
+  shape: 150,
+  trails: 3
+};
+
+gui.add(obj, "size").min(1).max(500);
+
+gui.add(obj, "amount").min(1).max(400);
+
+gui.add(obj, "speed").min(1).max(20);
+
+gui.add(obj, "shape").min(1).max(300);
+
+gui.add(obj, "trails").min(2).max(4);
+
+//p5 draw 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -12,40 +35,39 @@ function setup() {
 
 function gif(){
   
-push();
+  push();
 
-  blendMode(ADD);
-  
-  
-  for(var i=0; i<No; i++){
-    for(var n=0; n<tr; n++){
-      fill(i/tr,1,20/tr);
-      stroke(i/No,1,n/tr);
+    blendMode(ADD);
+    
+    
+    for(var i=0; i<obj.amount; i++){
+      for(var n=0; n<obj.trails; n++){
+        fill(i/obj.trails,1,20/obj.trails);
+        stroke(i/obj.amount,1,n/obj.trails);
+       
+        push();
+        
+        roun = TWO_PI+TWO_PI*i/obj.amount;
+        rotate(-roun);
+        r = obj.shape*sin(obj.shape/2*roun + TWO_PI*t + .1*n);
      
-      push();
-      
-      roun = TWO_PI*i/No;
-      rotate(-roun);
-      r = mouseX/2*sin(mouseY/2*roun + TWO_PI*t + .1*n);
-      
-      
-        beginShape();
-        vertex(r,100);
-        vertex((windowWidth/2),(windowHeight/2));
-        endShape(CLOSE);
+        
+          beginShape();
+          vertex(r,obj.size);
+          vertex((windowWidth/2),(windowHeight/2));
+          endShape(CLOSE);
 
-      pop();
+        pop();
+      }
     }
-  }
   pop();
 }
 
 
 function draw() {
-  t = counter*speed*5000;
+  t = counter*obj.speed*0.0000005;
   translate(width/2,height/2);
   background(0);
-  counter += 0.01;
+  counter += 1000;
   gif();
-  }
 }
