b66b4379215b8e1b384307d8887f8e7e579d6ac7
Putitontheground <ejsmith9725@gmail.com>
2017-10-19 13:09:42 +1300

    Audio File Input

--- sketch.js
+++ sketch.js
@@ -1,19 +1,22 @@
-
 var roun, r, t;
 var counter = 0;
-var mic;
+var song;
+var button;
 
+function preload() {
+    song = loadSound('assets/song.mp3');
+}
 
 //dat.GUI controls
 
 var gui = new dat.GUI();
 
 var obj = {
-  size: 150,
-  amount: 200,
-  speed: 10,
-  shape: 150,
-  trails: 3
+    size: 150,
+    amount: 200,
+    speed: 10,
+    shape: 150,
+    trails: 3
 };
 
 gui.add(obj, "size").min(1).max(500);
@@ -29,52 +32,46 @@ gui.add(obj, "trails").min(2).max(4);
 //p5 draw 
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  frameRate(60);
-  colorMode(HSB,1);
-  mic = new p5.AudioIn();
-  mic.start();
-
-
+    createCanvas(windowWidth, windowHeight);
+    frameRate(60);
+    colorMode(HSB, 1);
+    song.play();
 }
 
-function gif(){
-   var vol = mic.getLevel();
-  console.log(vol);
-  push();
+function gif() {
+
+    push();
 
     blendMode(ADD);
-    
-    
-    for(var i=0; i<obj.amount; i++){
-      for(var n=0; n<obj.trails; n++){
-        fill(i/obj.trails,1,20/obj.trails);
-        stroke(i/obj.amount,1,n/obj.trails);
-       
-        push();
-        
-        roun = TWO_PI*i/obj.amount;
-        rotate(-roun);
-        r = obj.shape*sin(obj.shape/2*roun + TWO_PI*t + .1*n);
-     
-        
-          beginShape();
-          vertex(r,obj.size*vol);
-          vertex((windowWidth/2),(windowHeight));
-          endShape(CLOSE);
 
-        pop();
-      }
+    for (var i = 0; i < obj.amount; i++) {
+        for (var n = 0; n < obj.trails; n++) {
+            fill(i / obj.trails, 1, 20 / obj.trails);
+            stroke(i / obj.amount, 1, n / obj.trails);
+
+            push();
+
+            roun = TWO_PI * i / obj.amount;
+            rotate(-roun);
+            r = obj.shape * sin(obj.shape / 2 * roun + TWO_PI * t + .1 * n);
+
+            beginShape();
+            vertex(r, obj.size * vol);
+            vertex((windowWidth / 2), (windowHeight));
+            endShape(CLOSE);
+
+            pop();
+        }
     }
-  pop();
+    pop();
 }
 
-
 function draw() {
-  t = counter*obj.speed*0.0000005;
-  translate(width/2,height/2);
-  background(0);
-  counter += 1000;
-  gif();
+    t = counter * obj.speed * 0.0000005;
+    translate(width / 2, height / 2);
+    background(0);
+    counter += 1000;
+    gif();
+    var vol = 1;
 
 }
