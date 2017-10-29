1d24271e6f5520824247533d068144a1ffbedf3e
Putitontheground <ejsmith9725@gmail.com>
2017-10-30 09:57:30 +1300

    Final Version
    libraries added also, if you want to run it from the files in phoenix you will need a local server

--- sketch.js
+++ sketch.js
@@ -1,122 +1,92 @@
-var roun;
-var r;
-var t;
-var counter = 0;
-var tracking;
 
-//dat.GUI controls
+            //dat.GUI controls
+            var No = 50, tr = 8;
+            var roun, r, t;
+            var speed = 1 / 10000;
+            var counter = 0;
 
-var gui = new dat.GUI();
+            //dat.gui interface
 
-var obj = {
-    size: 150,
-    amount: 200,
-    speed: 10,
-    shape: 150,
-    trails: 3
-};
+            function setup() {
+                createCanvas(windowWidth, windowHeight);
+                frameRate(10);
+                colorMode(HSB, 1);
 
-//dat.gui interface
+            }
 
-gui.add(obj, "size").min(1).max(500);
+            //color tracker
 
-gui.add(obj, "amount").min(1).max(400);
+            function gif() {
 
-gui.add(obj, "speed").min(1).max(20);
+                push();
 
-gui.add(obj, "shape").min(1).max(300);
+                blendMode(ADD);
 
-gui.add(obj, "trails").min(2).max(4);
+                for (var i = 0; i < No; i++) {
+                    for (var n = 0; n < tr; n++) {
+                        fill(i / tr, 1, 20 / tr);
+                        stroke(i / No, 1, n / tr);
 
-//p5 draw 
+                        push();
 
-function setup() {
-    createCanvas(windowWidth, windowHeight);
-    frameRate(60);
-    colorMode(HSB, 1);
+                        roun = TWO_PI * i / No;
+                        rotate(-roun);
+                        r = faceX / 2 * sin(faceY / 2 * roun + TWO_PI * t + .1 * n);
 
-}
+                        beginShape();
+                        vertex(r, 100);
+                        vertex((windowWidth / 2), (windowHeight / 2));
+                        endShape(CLOSE);
 
-//color tracker
+                        pop();
+                    }
+                }
+                pop();
+            }
 
-var colors = new tracking.ColorTracker('magenta', 'cyan', 'yellow');
+            function draw() {
+                t = counter * speed * 0.000005;
+                counter += 1000;
+                translate(width / 2, height / 2);
+                background(0);
+                gif();
 
-colors.on('track', function(event) {
-    if (event.data.length === 0) {// No colors were detected in this frame.
-    } else {
-        event.data.forEach(function(rect) {
-            console.log(rect.x, rect.y, rect.height, rect.width, rect.color);
-        });
-    }
-});
+            }
 
-tracking.track('#myVideo', colors);
+            var viewport = document.getElementById('viewport');
+            var canvas = document.getElementById('canvas');
+            var context = canvas.getContext('2d');
+            var faceX = 0;
+            var faceY = 0;
 
-window.onload = function() {
-      var canvas = document.getElementById('myVideo');
-      var context = canvas.getContext('2d');
-      tracking.ColorTracker.registerColor('purple', function(r, g, b) {
-        var dx = r - 120;
-        var dy = g - 60;
-        var dz = b - 210;
-        if ((b - g) >= 100 && (r - g) >= 60) {
-          return true;
-        }
-        return dx * dx + dy * dy + dz * dz < 3500;
-      });
-      var tracker = new tracking.ColorTracker(['yellow', 'purple']);
-      tracker.setMinDimension(5);
-      tracking.track('#video', tracker);
-      tracker.on('track', function(event) {
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        event.data.forEach(function(rect) {
-          if (rect.color === 'custom') {
-            rect.color = tracker.customColor;
-          }
-          context.strokeStyle = rect.color;
-          context.strokeRect(rect.x, rect.y, rect.width, rect.height);
-          context.font = '11px Helvetica';
-          context.fillStyle = "#fff";
-          context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
-          context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
-        });
-      });
-      initGUIControllers(tracker);
-    };
+            ;
+            var tracker = new tracking.ObjectTracker('face');
 
-function gif() {
-
-    push();
-
-    blendMode(ADD);
-
-    for (var i = 0; i < obj.amount; i++) {
-        for (var n = 0; n < obj.trails; n++) {
-            fill(i / obj.trails, 1, 20 / obj.trails);
-            stroke(i / obj.amount, 1, n / obj.trails);
-
-            push();
-
-            roun = TWO_PI * i / obj.amount;
-            rotate(-roun);
-            r = obj.shape * sin(obj.shape / 2 * roun + TWO_PI * t + .1 * n);
-
-            beginShape();
-            vertex(r, obj.shape);
-            vertex((windowWidth / 2), (windowHeight / 1.15));
-            endShape(CLOSE);
-
-            pop();
-        }
-    }
-    pop();
-}
-
-function draw() {
-    t = counter * obj.speed * 0.0000005;
-    counter += 1000;
-    translate(width / 2, height / 2);
-    background(0);
-    gif();
-
-}
+            tracker.setInitialScale(4);
+            tracker.setStepSize(2);
+            tracking.track('#video', tracker, {
+                camera: true
+            });
+            tracker.on('track', function(event) {
+                var maxRectArea = 0;
+                var maxRect;
+                event.data.forEach(function(rect) {
+                    if (rect.width * rect.height > maxRectArea) {
+                        maxRectArea = rect.width * rect.height;
+                        maxRect = rect;
+                    }
+                    context.clearRect(0, 0, canvas.width, canvas.height);
+                    context.strokeStyle = 'magenta';
+                    context.strokeRect(rect.x, rect.y, rect.width, rect.height);
+                    context.font = '11px Helvetica';
+                    context.fillStyle = "#fff";
+                    context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
+                    context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
+                });
+                if (maxRectArea > 0) {
+                    var rectCenterX = maxRect.x + (maxRect.width / 2);
+                    var rectCenterY = maxRect.y + (maxRect.height / 2);
+                    faceX = (rectCenterX - 160) * (window.innerWidth / 320) * 50;
+                    faceY = (rectCenterY - 120) * (window.innerHeight / 240) * 50;
+                }
+            });
